8316575075c73b46bce01c3bc6edfa3b21071113
Lilu <KHussell@gmail.com>
2019-07-20 17:16:00 -0400

    Updates

---
+++ precache-manifest.bfb3003ea2f1f25157c863d28a14b159.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "69e02f14dc0a3f485fce60a6fc33c6c0",
+    "url": "/memory2/index.html"
+  },
+  {
+    "revision": "9634a2804d5597ac62b3",
+    "url": "/memory2/static/js/2.548e0644.chunk.js"
+  },
+  {
+    "revision": "7f43a7532fb5bbb8f554",
+    "url": "/memory2/static/js/main.eee64641.chunk.js"
+  },
+  {
+    "revision": "2a08c075fa8b0e722505",
+    "url": "/memory2/static/js/runtime~main.9e724a24.js"
+  }
+]);
